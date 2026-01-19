9d69a083b56c35f692819b9304c0846a77e6e6d4
unknown <ibrahimcadabra1@gmail.com>
2026-01-19 13:59:03 +0300

    Enable API token auth endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('users/', views.UserListView.as_view(), name='users_list'),
     path('users/create/', views.UserCreateView.as_view(), name='user_create'),
     path('users/edit/<int:pk>/', views.UserEditView.as_view(), name='user_edit'),
